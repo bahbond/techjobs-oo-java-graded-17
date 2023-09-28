a8842c3d72d4d711cf10ba90c267536d80f99d1c
bahbond <bondopicks@gmail.com>
2023-09-28 13:22:55 -0500

    Test a new job

--- src/main/java/org/launchcode/techjobs/oo/Main.java
+++ src/main/java/org/launchcode/techjobs/oo/Main.java
@@ -6,12 +6,13 @@ public class Main {
              Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+        Job job4 = new Job("Groundskeeper", new Employer("StLouisCardinals"), new Location("St. Louis"),new PositionType("full time"), new CoreCompetency("Knowledge") );
 
         ArrayList<Job> jobs = new ArrayList<>();
         jobs.add(job1);
         jobs.add(job2);
         jobs.add(job3);
-
+        jobs.add(job4);
         for (Job job : jobs){
             System.out.println(job);
         }
